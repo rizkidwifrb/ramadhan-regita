3b2ea68c38fe427c9e648b712f0b20ba5b0b51c9
Rizki Dwi Febriansyah <rizkidwifrb@gmail.com>
2026-03-01 15:39:57 +0000

    premium ramadhan version

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function showMessage() {
-    document.getElementById("message").style.opacity = 1;
+const text =
+"Selamat menunaikan ibadah Ramadhan.\nSemoga hatimu selalu tenang,\ndan bahagia di setiap langkah.\n\nMaaf untuk semua masa lalu.";
+
+let i = 0;
+
+function typeWriter(){
+if(i < text.length){
+document.getElementById("typing").innerHTML += text.charAt(i);
+i++;
+setTimeout(typeWriter,40);
+}
+}
+
+function openLetter(){
+
+document.querySelector(".letter").style.opacity=1;
+
+document.getElementById("music").play();
+
+typeWriter();
 }
